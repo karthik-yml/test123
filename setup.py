3f940683e941eb874eb08fa7a6783ae5d5e59cd0
karthik-yml <karthik.tv@ymedialabs.com>
2023-11-25 11:19:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test123',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='karthik',
+      author_email='karthiktv2001@gmail.com',
+      license='MIT',
+      packages=['test123'],
+      zip_safe=False)
